74c79716dd64902ecbfc2edac95ff682c0a5f108
r1tter <marcelosori@hotmail.com>
2023-09-15 15:27:13 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import { fastify } from 'fastify'
+import { getAllPromptsRoute } from './routes/get-all-prompts'
+
+const app = fastify()
+
+app.register(getAllPromptsRoute)
+
+app.listen({
+  port: 3333,
+}).then(() => {
+  console.log('listening on port 3333')
+})
